0b3b6a3e36562130fa158ed79e3a59f52955d008
User <user@Users-MacBook-Pro-3.local>
2018-12-19 14:39:00 -0800

    movie finder commit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,38 @@
-const express = require('express');
 
 
-const app = express();
+var express=require('express');
+var morgan = require('morgan');
+var app = express();
+var axios = require('axios');
+var movieData = {};
+var movieUrl = 'http://www.omdbapi.com';
+var apiKey = '&apikey=8730e0e';
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    var urlOmdb = movieUrl + req.url + apiKey;
+
+    if ( movieData[req.url] === undefined ) { 
+axios.get(urlOmdb)
+
+.then( (axiosres) => {
+
+movieData[req.url] = axiosres.data;
+
+    
+res.json(movieData[req.url]);            
+
+ 
+})      
+.catch( (error) => {
+
+})
+
+} else {
+res.json(movieData[req.url]);
+
+}
+})  
 
 module.exports = app;
